5ca73e834b9d0f6f1b8f3fa99b4f9d7c85cbe421
BetsyBiegon <betsybiegon@gmail.com>
2024-06-12 20:11:37 +0300

    try

--- database/init_db.py
+++ database/init_db.py
@@ -1,5 +1,5 @@
 from database import engine, Base
-from models.book import Book
+from models.books import Book
 from sqlalchemy.orm import sessionmaker
 
 # Create the database tables
@@ -25,7 +25,20 @@ books = [
     Book(title="Brave New World", author="Aldous Huxley"),
     Book(title="The Odyssey", author="Homer"),
     Book(title="The Divine Comedy", author="Dante Alighieri"),
-    Book(title="Don Quixote", author="Miguel de Cervantes")
+    Book(title="Don Quixote", author="Miguel de Cervantes"),
+    Book(title="A song of ice and fire", author="George R.R. Martin"),
+    Book(title="The Lord of the Rings", author="J.R.R. Tolkien"),
+    Book(title="The Adventures of Huckleberry Finn", author="Mark Twain"),
+    Book(title="The Harry Potter", author="J.K. Rowling"),
+    Book(title="The Chronicles of Narnia", author="J.K. Rowling"),
+    Book(title="The Lion, the Witch, and the Wardrobe", author="J.K. Rowling"),
+    Book(title="Fifty Shades of Grey", author="E.L. James"),
+    Book(title="Fifty Shades Darker", author="E.L. James"),
+    Book(title="Fifty Shades Freed", author="E.L. James"),
+    Book(title="The Da Vinci Code", author="Dan Brown"),
+    Book(title="The Alchemist", author="Paulo Coelho"),
+    Book(title="The Girl with the Dragon Tattoo", author="Harper Lee"),
+
 ]
 
 # Add the books to the session and commit
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from .book import Book
+from models.books import Book
